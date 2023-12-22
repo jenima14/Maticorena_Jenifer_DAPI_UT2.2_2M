4175af4fba8f81dbdcdad8ec23a7126e4dd8c3e9
jenima14 <jmarticarr2@educacion.navarra.es>
2023-12-22 10:32:04 +0100

    jkln

---
+++ ejercicio_02.py
@@ -0,0 +1,15 @@
+datos = {"tiempo":"km", "corredor":""}
+while true:
+    diccionario= int(input("Ingresa la hora: "))
+    if diccionario =
+        
+    
+
+
+
+
+#diccionario = {"tiempo":"km", "corredor":""}
+#for diccionario in datos(len(diccionario))
+
+
+
